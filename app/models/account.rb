e477ef686fbfb29839a13e7759a9e825b2f3ef64
Rien Maertens <rien.maertens@posteo.be>
2022-05-20 22:53:15 +0200

    Automatically annotate models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id          :bigint           not null, primary key
+#  bank        :string
+#  name        :string
+#  uid         :string
+#  valid_until :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Account < ApplicationRecord
   encrypts :uid
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id          :bigint           not null, primary key
+#  bank        :string
+#  name        :string
+#  uid         :string
+#  valid_until :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
